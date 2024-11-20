410f2b11c13073d7774ffa7f436b0a5e0126bd35
Octowl <alsaff1987@gmail.com>
2024-11-20 14:06:03 +0300

    morgan and error handling

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const { authRouter } = require("./routes/auth");
 const { postsRouter } = require("./routes/posts");
+const { handleErrors } = require("./middleware");
 
 const app = express();
 
@@ -9,11 +11,17 @@ const app = express();
  * Middleware
  */
 app.use(express.json());
+app.use(morgan("dev"));
 
 /**
  * Routers
  */
 app.use("/auth", authRouter);
 app.use("/posts", postsRouter);
+
+/**
+ * Error Handling
+ */
+app.use(handleErrors);
 
 module.exports = app;
