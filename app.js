20190abbd03b6e0c405303abda3fb1c42e57f674
Octowl <alsaff1987@gmail.com>
2024-11-20 13:58:47 +0300

    add body middleware
    Co-authored-by: Kuwait-ZERO <Kuwait-ZERO@users.noreply.github.com>
    Co-authored-by: HUSSAIN13Q <HUSSAIN13Q@users.noreply.github.com>
    Co-authored-by: Eng-Batol <Eng-Batol@users.noreply.github.com>
    Co-authored-by: 7336x <7336x@users.noreply.github.com>
    Co-authored-by: Khadeejahismaeil <Khadeejahismaeil@users.noreply.github.com>
    Co-authored-by: meshariSalhouli <meshariSalhouli@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+
 const { authRouter } = require("./routes/auth");
+const { postsRouter } = require("./routes/posts");
 
 const app = express();
 
+/**
+ * Middleware
+ */
+app.use(express.json());
+
+/**
+ * Routers
+ */
 app.use("/auth", authRouter);
+app.use("/posts", postsRouter);
 
 module.exports = app;
