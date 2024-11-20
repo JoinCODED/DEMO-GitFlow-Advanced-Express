d8b998f7f6a6472d478fe704eb67c34513596894
Octowl <alsaff1987@gmail.com>
2024-11-20 11:15:11 +0300

    setup an express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+
+const start = async () => {
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+start();
