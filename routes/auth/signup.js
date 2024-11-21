cecd31ce4be88959b58435f274cab2619eeb2c10
Octowl <alsaff1987@gmail.com>
2024-11-21 08:56:26 +0300

    generate token
    Co-authored-by: Kuwait-ZERO <Kuwait-ZERO@users.noreply.github.com>
    Co-authored-by: meshariSalhouli <meshariSalhouli@users.noreply.github.com>
    Co-authored-by: Eng-Batol <Eng-Batol@users.noreply.github.com>
    Co-authored-by: GhanimAlhashash <GhanimAlhashash@users.noreply.github.com>
    Co-authored-by: 7336x <7336x@users.noreply.github.com>
    Co-authored-by: HUSSAIN13Q <HUSSAIN13Q@users.noreply.github.com>
    Co-authored-by: Khadeejahismaeil <Khadeejahismaeil@users.noreply.github.com>

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,23 +1,42 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const { body } = require("express-validator");
 
 // const Post = require("../../models/Post");
 const validateRequest = require("../../middleware/validateRequest");
+const User = require("../../models/User");
+const { BadRequestError } = require("../../errors");
 
 const router = express.Router();
 
 const validators = [
-  body("title").not().isEmpty().withMessage("Title is required"),
-  body("content").not().isEmpty().withMessage("Content is required"),
+  body("username").not().isEmpty().withMessage("Username is required"),
+  body("password")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Password must be between 4 and 20 characters"),
 ];
 
 router.post("/signup", validators, validateRequest, async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check for existing user
+  const existingUser = await User.findOne({ username });
+
+  if (existingUser) throw BadRequestError("Username is taken");
+
   // Create a user
+  const user = await User.create({ username, password });
 
   // Generate a token
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRATION }
+  );
 
   // Respond with the token
-  res.status(201).json({});
+  res.status(201).json({ token });
 });
 
 module.exports = { signupRouter: router };
