119486094002b2960a671955cddb25d8f271234b
Octowl <alsaff1987@gmail.com>
2025-01-16 11:36:48 +0300

    remove posts from model

--- models/User.js
+++ models/User.js
@@ -6,12 +6,6 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  posts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Post",
-    },
-  ],
 });
 
 UserSchema.pre("save", async function (done) {
