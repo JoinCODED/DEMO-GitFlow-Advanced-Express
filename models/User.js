5aaaef464a2d6455b89dfcdf9f8e2f0f1c80e188
Octowl <alsaff1987@gmail.com>
2025-01-16 12:05:31 +0300

    remove password from User json

--- models/User.js
+++ models/User.js
@@ -2,11 +2,23 @@ const { model, Schema } = require("mongoose");
 
 const PasswordManager = require("../helpers/PasswordManager");
 
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
+  }
+);
 
 UserSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
