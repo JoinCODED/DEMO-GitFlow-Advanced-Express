6fa848d9e866dcb005ee786da3ca7a05ce53ed56
TariqBazadough <tariq.bazadough@gmail.com>
2025-01-19 14:23:10 +0300

    done with get note

--- routes/notes/index.js
+++ routes/notes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { noteCreateRouter } = require("./createNote");
+const {noteGetRouter} = require("noteGet")
 
 const router = express.Router();
 
 router.use(noteCreateRouter);
+router.use(noteGetRouter);
 
 module.exports = { notesRouter: router };
---
+++ routes/notes/noteGet.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const findNote = require("./findNote");
+
+const router = express.Router();
+
+router.param("id", findNote);
+
+router.get("/:id", (req, res) => {
+  res.status(200).json(req.post);
+});
+
+module.exports = { noteGetRouter: router };
